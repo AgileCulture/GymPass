12ad428582df1f2cb09acb390b31aa904a28c051
Shane Moran <shane.moran.2@ucdconnect.ie>
2016-11-26 19:35:07 +0000

    Shane - added Voice to Share

--- app/src/main/java/com/example/jakubkurtiak/gympass/ShareActivity.java
+++ app/src/main/java/com/example/jakubkurtiak/gympass/ShareActivity.java
@@ -4,6 +4,7 @@ package com.example.jakubkurtiak.gympass;
         import android.database.Cursor;
         import android.graphics.Typeface;
         import android.net.Uri;
+        import android.speech.RecognizerIntent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.widget.TextView;
@@ -12,14 +13,25 @@ package com.example.jakubkurtiak.gympass;
         import android.view.View;
         import android.widget.Button;
 
+        import com.google.android.gms.appindexing.Action;
+        import com.google.android.gms.appindexing.AppIndex;
+        import com.google.android.gms.appindexing.Thing;
+        import com.google.android.gms.common.api.GoogleApiClient;
+
         import java.text.ParseException;
         import java.text.SimpleDateFormat;
+        import java.util.ArrayList;
         import java.util.Date;
-        import java.util.HashMap;
-        import java.util.Hashtable;
+
 
 
 public class ShareActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +39,17 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_share);
 
 
-        CommonMethods.setImpactFont(ShareActivity.this,R.id.top,R.string.gympass);
-        CommonMethods.setImpactFont(ShareActivity.this,R.id.share_now,R.string.share_now);
-        CommonMethods.setImpactFont(ShareActivity.this,R.id.button,R.string.button);
-        CommonMethods.setImpactFont(ShareActivity.this,R.id.button2,R.string.button2);
-        CommonMethods.setImpactFont(ShareActivity.this,R.id.button3,R.string.button3);
+        CommonMethods.setImpactFont(ShareActivity.this, R.id.top, R.string.gympass);
+        CommonMethods.setImpactFont(ShareActivity.this, R.id.share_now, R.string.share_now);
+        CommonMethods.setImpactFont(ShareActivity.this, R.id.button, R.string.button);
+        CommonMethods.setImpactFont(ShareActivity.this, R.id.button2, R.string.button2);
+        CommonMethods.setImpactFont(ShareActivity.this, R.id.button3, R.string.button3);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+    // Constant to use for the reequest coded needed for VC
+    public static final int VC_REQUEST_CODE = 7777;
 
     private String getChooseAppString() {
         return getString(R.string.share_to_app);
@@ -40,28 +57,31 @@ public class ShareActivity extends AppCompatActivity {
     }
 
 
-    public void shareStatus(View view) {
+
+    public void shareStatus() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-
         String msgToShare = "Working out at the gym - sent from GymPass";
         shareIntent.putExtra(Intent.EXTRA_TEXT, msgToShare);
         startActivity(Intent.createChooser(shareIntent, getChooseAppString()));
     }
 
     public void shareUpdate(View view) {
+        /*
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-
         // CommonMethods.timeSinceLastVisit(ShareActivity.this);
-
         String msgToShare = "In here will go an update: I have been at the gym X times in the past Y days/weeks/months";
         shareIntent.putExtra(Intent.EXTRA_TEXT, msgToShare);
         startActivity(Intent.createChooser(shareIntent, getChooseAppString()));
+        */
+
+        startVoiceRecognitionActivity();
+
     }
 
 
-    public void shareAll(View view) {
+    public void shareAll() {
         Intent shareAllIntent = new Intent(Intent.ACTION_SEND);
         shareAllIntent.setType("text/plain");
         int totalVisits = CommonMethods.readNumberOfVisits(ShareActivity.this);
@@ -73,6 +93,60 @@ public class ShareActivity extends AppCompatActivity {
         shareAllIntent.putExtra(Intent.EXTRA_TEXT, msgToShareAll);
         startActivity(Intent.createChooser(shareAllIntent, getChooseAppString()));
     }
+
+
+
+
+
+    // Inspired by a good SO answer at: stackoverflow.com/questions/11798337/how-to-voice-commands-into-an-android-application
+    public void startVoiceRecognitionActivity() {
+        Intent vcIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        vcIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        vcIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say \"Status\" or \"All\" to share which update version you'd prefer:");
+        startActivityForResult(vcIntent, VC_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VC_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> matchedWords = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+
+
+            String listOfWords = matchedWords.get(0);
+            int count = 0;
+            for (String splitUp: listOfWords.split(" ")) { count++; }
+            String [] splitMatches = new String[count];
+            int splitMatchesIndex = 0;
+            for (String singleWord: listOfWords.split(" ")) {
+                splitMatches[splitMatchesIndex] = singleWord;
+                splitMatchesIndex++;
+            }
+
+            if (matchedWords.contains("status") || matchedWords.contains("stats") || matchedWords.contains("stars")) {
+                shareStatus();
+            }
+            else if (matchedWords.contains("all") || matchedWords.contains("oil") || matchedWords.contains("owl") || matchedWords.contains("call")) {
+                shareAll();
+            }
+
+            else {
+                // open a basic TextView with list of matched words
+                setContentView(R.layout.array_words);
+                setContenttoListArrayWords(listOfWords);
+            }
+        }
+
+    }
+
+    private void setContenttoListArrayWords(String listOfWords) {
+        TextView view = (TextView) findViewById(R.id.array_words_content);
+        view.setText("The words Android Voice-Recognition picked up were:\n\n" + listOfWords);
+        view.setTextColor(getResources().getColor(R.color.black));
+    }
+
+
 
 
     // Customised DB methods, with accessibility to the methods set as: private to this Class
@@ -99,30 +173,62 @@ public class ShareActivity extends AppCompatActivity {
         // into long. Then time of last visit is subtracted from actual time which
         // gives us the actual time since last visit.
         // This time is in seconds, so must be changed to human readable format.
-        long dateTimeNowInSeconds = new Date().getTime()/1000;
-        long dateTimeFirstVisitInSeconds = dateOfFirstVisit.getTime()/1000;
+        long dateTimeNowInSeconds = new Date().getTime() / 1000;
+        long dateTimeFirstVisitInSeconds = dateOfFirstVisit.getTime() / 1000;
         long timeSinceLastVisitInSeconds = dateTimeNowInSeconds - dateTimeFirstVisitInSeconds;
         int daysSinceFirstVisit = (int) (timeSinceLastVisitInSeconds / 86400);
 
         // Returns a String of how long since user's first gym session, formatted in days or weeks.
         String formattedPeriodSinceFirstVisit;
 
-            if (daysSinceFirstVisit == 1) {
-                formattedPeriodSinceFirstVisit = "1 day";
-            }
-            else if (daysSinceFirstVisit < 7) {
-                formattedPeriodSinceFirstVisit = daysSinceFirstVisit + " days";
-            }
-            else if (daysSinceFirstVisit == 7) {
-                formattedPeriodSinceFirstVisit = "1 week";
-            }
-            else {
-                int weeksSinceFirstVisit = daysSinceFirstVisit / 7;
-                formattedPeriodSinceFirstVisit = weeksSinceFirstVisit + " weeks";
-            }
+        if (daysSinceFirstVisit == 1) {
+            formattedPeriodSinceFirstVisit = "1 day";
+        } else if (daysSinceFirstVisit < 7) {
+            formattedPeriodSinceFirstVisit = daysSinceFirstVisit + " days";
+        } else if (daysSinceFirstVisit == 7) {
+            formattedPeriodSinceFirstVisit = "1 week";
+        } else {
+            int weeksSinceFirstVisit = daysSinceFirstVisit / 7;
+            formattedPeriodSinceFirstVisit = weeksSinceFirstVisit + " weeks";
+        }
 
         return formattedPeriodSinceFirstVisit;
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Share Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
