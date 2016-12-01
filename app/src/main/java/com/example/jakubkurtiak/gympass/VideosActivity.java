4cc52e4243f6d0c6ce1f176a29717f7942aef1af
Jakub Kurtiak <jakub@kurtiak.pl>
2016-12-01 16:30:36 +0000

    Refinements in VideoActivity.

--- app/src/main/java/com/example/jakubkurtiak/gympass/VideosActivity.java
+++ app/src/main/java/com/example/jakubkurtiak/gympass/VideosActivity.java
@@ -19,32 +19,31 @@ public class VideosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
 
-        // ***  BEFORE REMOVING THE BELOW COMMON METHODS, REMEMBER TO setImpactFont() ON THE BUTTON TEXT IN 3 METHODS BELOW
+        // Populating text on screen elements.
         CommonMethods.awardBadge(VideosActivity.this, R.id.badge);
-        CommonMethods.setImpactFont(VideosActivity.this,R.id.top,R.string.gympass);
-        CommonMethods.setImpactFont(VideosActivity.this,R.id.videos,R.string.button_videos);
-        CommonMethods.setImpactFont(VideosActivity.this,R.id.classes_text,R.string.classes_text);
-        CommonMethods.setImpactFont(VideosActivity.this,R.id.classes_bottom_text,R.string.classes_bottom);
-
+        CommonMethods.setImpactFont(VideosActivity.this, R.id.top, R.string.gympass);
+        CommonMethods.setImpactFont(VideosActivity.this, R.id.videos, R.string.button_videos);
+        CommonMethods.setImpactFont(VideosActivity.this, R.id.classes_text, R.string.video_text_ucd_induction_video);
+        CommonMethods.setImpactFont(VideosActivity.this, R.id.classes_text1, R.string.video_text_pre_workout);
+        CommonMethods.setImpactFont(VideosActivity.this, R.id.classes_text2, R.string.video_text_bench_press);
+        CommonMethods.setImpactFont(VideosActivity.this, R.id.classes_text3, R.string.video_text_ucd_marketing);
     }
 
     protected void playClassesVideo1(View view){
-        playVideo("https://www.youtube.com/watch?v=1drXLfIT7p8");
+        playVideo("https://www.youtube.com/watch?v=FRfmw3jw2c4");
     }
 
     protected void playClassesVideo2(View view){
-        playVideo("https://www.youtube.com/watch?v=44861zB1Bao");
+        playVideo("https://www.youtube.com/watch?v=nN8cwK3zM20");
     }
 
     protected void playClassesVideo3(View view){
-        playVideo("https://www.youtube.com/watch?v=vtIQcIMr7iM");
+        playVideo("https://www.youtube.com/watch?v=UaOwz6DNdjw");
     }
 
-
-    /* Youtube link to UCD Induction video  - comment left by Shane
-         https://www.youtube.com/watch?v=FRfmw3jw2c4
-     */
-
+    protected void playClassesVideo4(View view){
+        playVideo("https://www.youtube.com/watch?v=xZXrXIkGrx4");
+    }
 
     // Simple intent to play YouTube video after clicking on an image/button.
     protected void playVideo(String video) {
